d201b4274087f4fb512c43fa4350107571417203
gitkewl <geddie81@gmail.com>
2013-08-10 18:56:42 -0400

    Updated file

--- backend.sh
+++ backend.sh
@@ -4,8 +4,8 @@
 
 # Configure Package Management System (APT)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-cp /setup/10gen.list /etc/apt/sources.list.d/
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+sudo apt-get update
 
 # Install MongoDB
-sudo apt-get udpdate
 sudo apt-get install mongodb-10gen
