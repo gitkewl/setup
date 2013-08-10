93f27383f329c194764bfbe3b60ab71480bef903
gitkewl <geddie81@gmail.com>
2013-08-10 12:35:28 -0400

    Init

---
+++ backend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Shell script for installation and configuring of MongoDB
+# on an Ubuntu 12.04 LTS EC2 instance for headless setup. 
+
+# Configure Package Management System (APT)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
+sudo apt-get -y update
+
+# Install MongoDB
+sudo apt-get -y install mongodb-10gen
+
+#Start MongoDB
+sudo /etc/init.d/mongodb start
