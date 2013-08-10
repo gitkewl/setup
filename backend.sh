ca8ce7bd6572b9a633634bbbab745a8ad0bcf24a
gitkewl <geddie81@gmail.com>
2013-08-10 18:19:34 -0400

    Update file

--- backend.sh
+++ backend.sh
@@ -4,8 +4,8 @@
 
 # Configure Package Management System (APT)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee -a /etc/apt/sources.list.d/10gen.list
+cp /setup/10gen.list /etc/apt/sources.list.d/
 
 # Install MongoDB
-sudo apt-get update
+sudo apt-get udpdate
 sudo apt-get install mongodb-10gen
